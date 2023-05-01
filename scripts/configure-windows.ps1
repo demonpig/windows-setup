682c1a8b45eaade9b7eca230639bdd1ec74848a0
Max Mitschke <maxmitschke@fastmail.com>
2023-05-01 10:33:50 -0400

    Update script to really enable remote desktop

--- scripts/configure-windows.ps1
+++ scripts/configure-windows.ps1
@@ -1,6 +1,8 @@
 # configure-windows.ps1
 # This script should be executed by sysprep during the specialize pass.
 
-Enable-NetFirewallRule -name RemoteDesktop-UserMode-In-TCP
+# The following will enable Remote Desktop
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 
 winrm quickconfig -force
